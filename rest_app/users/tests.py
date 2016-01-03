a7209bfb4fe92678f1b1a1ffc2df21933e5239ec
Tally Osborne <tallyoh@gmail.com>
2016-01-02 22:52:57 -0500

    Updated user model to make associations on save, added test.

--- rest_app/users/tests.py
+++ rest_app/users/tests.py
@@ -1,5 +1,17 @@
 from django.test import TestCase
+from users.models import VerbUser
+from verbs.models import Tense, Infinitive
 
-# Create your tests here.
-# User needs one of conjugation, gerund, participle set
-# No more than one can be set
+class BasicModelTestCase(TestCase):
+  fixtures = ['verbs/fixtures/initial_data.json']
+
+  def test_creating_user_creates_defaults(self):
+    user = VerbUser.objects.create(password='1234', username='tallyoh', email='tally@test.com')
+    user.save()
+    self.assertNotEqual(user.pk, None)
+
+    infinitives = Infinitive.objects.filter(from_duolingo = True)
+    tenses = Tense.objects.filter(default = True)
+
+    self.assertEqual(user.infinitives.count(), infinitives.count())
+    self.assertEqual(user.tenses.count(), tenses.count())
